eb3855baecf03975f6aec506dd2899a31463c198
reut <reutedry1@gmail.com>
2025-01-02 01:36:14 +0200

    Fix mentor + codeValue sockets + add comments

--- services/socket.service.js
+++ services/socket.service.js
@@ -4,6 +4,11 @@ import { Server } from 'socket.io'
 var gIo = null
 var connectedUsers = {}
 var mentorId
+
+// The blockValue variable stores the current state of each code block.
+// This ensures that when a new user joins a block after content has been written,
+// they can immediately receive the complete content without needing multiple HTTP requests.
+// Keeping the block data within the socket service ensures real-time consistency and minimizes the need for additional backend interactions.
 var blockValue = {}
 
 export function setupSocketAPI(http) {
@@ -13,15 +18,16 @@ export function setupSocketAPI(http) {
         }
     })
 
-    ////////// REUT
-
     gIo.on('connection', socket => {
-
         logger.info(`New connected socket [id: ${socket.id}]`)
 
         socket.on('disconnect', () => {
-            // when the page refresh it is diconnecting and this code removes the socket so it wont double added
             const block = socket.myBlock
+
+            // When the mentor refreshes the page and disconnects, their socket is removed.Since the mentor is essential for the block, all connected users in the block are notified and disconnected to maintain consistency.
+            setMentorLeave(socket, block)
+
+            // When a user refreshes the page, their socket disconnects. To prevent duplicate entries, we ensure that the socket is properly removed from the connectedUsers list for the associated block
             if (block && connectedUsers[block]) {
                 connectedUsers[block] = connectedUsers[block].filter(userId => userId !== socket.id)
                 console.log('connectedUsers when disconnect', connectedUsers[block]);
@@ -30,14 +36,13 @@ export function setupSocketAPI(http) {
 
                 logger.info(`Socket disconnected [id: ${socket.id}] from block ${block}`)
             }
+
         })
 
         socket.on('set-block', block => {
-            console.log('set-block')
 
-            if (socket.myBlock === block) return
-
-            // if the socket has block so i want to disconnect it and then join it
+            // If the socket is already connected to a block, remove it from the current block
+            // before joining the new block to avoid duplicate connections.
             if (socket.myBlock) {
                 const oldBlock = socket.myBlock
                 removeUserFromBlock(socket, oldBlock)
@@ -46,43 +51,46 @@ export function setupSocketAPI(http) {
             socket.join(block)
             socket.myBlock = block
 
+
+            // Initialize the connectedUsers array for the block if it doesn't exist,
+            // then add the current socket ID to the list of connected users for the block.
             if (!connectedUsers[socket.myBlock]) connectedUsers[socket.myBlock] = []
             connectedUsers[socket.myBlock].push(socket.id)
 
-            // the first person comes into the room
-            if (connectedUsers[socket.myBlock].length === 1) {
+            // Assign the mentor role to the first user who enters the block (if no mentor exists).
+            if (!mentorId) {
                 mentorId = socket.id
                 socket.emit('is-mentor', true)
             } else {
                 socket.emit('is-mentor', false)
             }
-            // else if (mentorId === socket.id) { // if the socket thats connect is the mentor
-            //     socket.emit('is-mentor', true)
-            // checking how many users are connected
+
+            // If the block already contains written code, send it to the newly joined user
+            // so they can view the current state of the code block.
             if (blockValue[socket.myBlock]) {
-                socket.broadcast.to(socket.myBlock).emit('code-block-add', blockValue[socket.myBlock])
+                socket.emit('code-block-add', blockValue[socket.myBlock])
             }
+
+            // Notify all users in the block about the updated count of connected users.
             gIo.to(block).emit('connected-users-count', connectedUsers[socket.myBlock].length)
         })
 
         socket.on('leave-block', block => {
             if (socket.myBlock === block) {
-                removeUserFromBlock(socket, block)
-                delete socket.myBlock
-            }
 
-            if (mentorId === socket.id) {
-                connectedUsers[block].forEach(userId => {
-                    gIo.to(userId).emit('mentor-leave', 'Mentor has left the block. You are being redirected.')
-                })
-                connectedUsers[block] = []
-                blockValue[block] = ''
-                mentorId = ''
+                // If the user leaving is the mentor, disconnect all users in the block using `setMentorLeave`. 
+                // This eliminates the need to first remove the mentor as a regular user and then handle the mentor logic.
+                if (mentorId === socket.id) {
+                    setMentorLeave(socket, block)
+                } else {
+                    removeUserFromBlock(socket, block)
+                    delete socket.myBlock
+                }
             }
-
         })
 
         socket.on('code-block-write', value => {
+            // Store the user's input (value) in blockValue to update the code block state.
             blockValue[socket.myBlock] = value
             socket.broadcast.to(socket.myBlock).emit('code-block-add', blockValue[socket.myBlock])
             logger.info(`Block updated from socket [id:${socket.id}]`)
@@ -93,40 +101,9 @@ export function setupSocketAPI(http) {
             logger.info(`Block updated from socket [id:${socket.id}]`)
         })
 
-
-
-        //////////// 
-
-
-
-
-        socket.on('chat-send-msg', msg => {
-            logger.info(`New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myTopic}`)
-            // emits to all sockets:
-            // gIo.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            gIo.to(socket.myTopic).emit('chat-add-msg', msg)
-        })
-
-        socket.on('user-watch', userId => {
-            logger.info(`user-watch from socket [id: ${socket.id}], on user ${userId}`)
-            socket.join('watching:' + userId)
-        })
-
-        socket.on('set-user-socket', userId => {
-            logger.info(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
-            socket.userId = userId
-        })
-
-        socket.on('unset-user-socket', () => {
-            logger.info(`Removing socket.userId for socket [id: ${socket.id}]`)
-            delete socket.userId
-        })
-
     })
 }
 
-//////// REUT
 function removeUserFromBlock(socket, block) {
     socket.leave(block)
     if (connectedUsers[block]) {
@@ -136,77 +113,18 @@ function removeUserFromBlock(socket, block) {
     }
 }
 
-////////
-
-function emitTo({ type, data, label }) {
-    if (label) gIo.to('watching:' + label.toString()).emit(type, data)
-    else gIo.emit(type, data)
-}
-
-async function emitToUser({ type, data, userId }) {
-    userId = userId.toString()
-    const socket = await _getUserSocket(userId)
-
-    if (socket) {
-        logger.info(`Emiting event: ${type} to user: ${userId} socket [id: ${socket.id}]`)
-        socket.emit(type, data)
-    } else {
-        logger.info(`No active socket for user: ${userId}`)
-        // _printSockets()
+function setMentorLeave(socket, block) {
+    if (mentorId === socket.id) {
+        connectedUsers[block].forEach(userId => {
+            gIo.to(userId).emit('mentor-leave', 'Mentor has left the block. You are being redirected.')
+        })
+        connectedUsers[block] = []
+        blockValue[block] = ''
+        mentorId = ''
     }
-}
-
-// If possible, send to all sockets BUT not the current socket 
-// Optionally, broadcast to a room / to all
-// async function broadcast({ type, data, room = null, userId }) {
-//     // userId = userId.toString()
-
-//     logger.info(`Broadcasting event: ${type}`)
-//     const excludedSocket = await _getUserSocket(userId)
-//     if (room && excludedSocket) {
-//         logger.info(`Broadcast to room ${room} excluding user: ${userId}`)
-//         excludedSocket.broadcast.to(room).emit(type, data)
-//     } else if (excludedSocket) {
-//         logger.info(`Broadcast to all excluding user: ${userId}`)
-//         excludedSocket.broadcast.emit(type, data)
-//     } else if (room) {
-//         logger.info(`Emit to room: ${room}`)
-//         gIo.to(room).emit(type, data)
-//     } else {
-//         logger.info(`Emit to all`)
-//         gIo.emit(type, data)
-//     }
-// }
-
-async function _getUserSocket(userId) {
-    const sockets = await _getAllSockets()
-    const socket = sockets.find(s => s.userId === userId)
-    return socket
-}
-
-async function _getAllSockets() {
-    // return all Socket instances
-    const sockets = await gIo.fetchSockets()
-    return sockets
-}
-
-async function _printSockets() {
-    const sockets = await _getAllSockets()
-    console.log(`Sockets: (count: ${sockets.length}):`)
-    sockets.forEach(_printSocket)
-}
-function _printSocket(socket) {
-    console.log(`Socket - socketId: ${socket.id} userId: ${socket.userId}`)
 }
 
 export const socketService = {
     // set up the sockets service and define the API
     setupSocketAPI,
-    // emit to everyone / everyone in a specific room (label)
-    emitTo,
-    // emit to a specific user (if currently active in system)
-    emitToUser,
-    // Send to all sockets BUT not the current socket - if found
-    // (otherwise broadcast to a room / to all)
-    // broadcast,
 }
