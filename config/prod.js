6c447c76d5fe6e29bec9eddf6fc16c132fc8751d
reut <reutedry1@gmail.com>
2025-01-02 02:14:51 +0200

    Set dotenv

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,4 @@ export default {
     dbURL: process.env.MONGO_URL,
     dbName: process.env.DB_NAME
 }
-// export default {
-//     dbURL: process.env.MONGO_URL || 'mongodb+srv://reutedry1:reutedry1@cluster0.7r1ws.mongodb.net/',
-//     dbName: process.env.DB_NAME || 'codeBlock_db'
-// }
+
